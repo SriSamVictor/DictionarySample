1660df99a60793e59bdda5131bd8dec87f3fa07f
System Administrator <root@Jean-Martin.local>
2018-06-22 12:44:42 +0530

    first commit

---
+++ DictSample/DictExample.h
@@ -0,0 +1,13 @@
+//
+//  DictExample.h
+//  DictSample
+//
+//  Created by Jean Martin on 20/06/18.
+//  Copyright © 2018 Public. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DictExample : NSObject
+
+@end
---
+++ DictSample/DictExample.m
@@ -0,0 +1,13 @@
+//
+//  DictExample.m
+//  DictSample
+//
+//  Created by Jean Martin on 20/06/18.
+//  Copyright © 2018 Public. All rights reserved.
+//
+
+#import "DictExample.h"
+
+@implementation DictExample
+
+@end
